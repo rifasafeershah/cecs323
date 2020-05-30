bbe5061092eed8fe597dd0ffe9a1d7eb26f7daf7
Rifa Safeer Shah <rifashah28@gmail.com>
2020-05-30 00:22:18 -0700

    Update FirstRecords.sql

--- labs/First Records/FirstRecords.sql
+++ labs/First Records/FirstRecords.sql
@@ -1,1 +1,13 @@
-
+CREATE TABLE FirstRecords (
+  FirstName VARCHAR(20) NOT NULL,
+  LastName VARCHAR(20) NOT NULL,
+  Email VARCHAR(20) NOT NULL,
+  Phone VARCHAR(20),
+  Task VARCHAR(20),
+  DueDate Date,
+  CONSTRAINTS FirstRecords_pk
+  PPIMARY KEY (FirstName, LastName, Phone)
+  );
+  
+INSERT INTO FirstRecords
+VALUES ('Tim', 'Ross', 'tim.ross@gmail.com', '(123)456-7890', 'Paint a Picture', 2020-30-5);
