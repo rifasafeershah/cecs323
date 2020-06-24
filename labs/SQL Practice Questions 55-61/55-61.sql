4ec534cd47763d342218338f118b52cc784a6f71
Rifa Safeer Shah <rifashah28@gmail.com>
2020-06-23 22:53:46 -0700

    Create 55-61.sql

---
+++ labs/SQL Practice Questions 55-61/55-61.sql
@@ -0,0 +1,14 @@
+55. Find customers who have ordered the same thing.  For instance, if ‘AV Stores, Co.’ orders a particular item five times, and ‘Land of Toys Inc.’ orders that same item 4 times, it only counts as one item that they have ordered in common.  Find only those customer pairs who have ordered at least 40 different items in common (3).
+      
+
+56. What is the manager who manages the greatest number of employees (2)
+
+57. Select all employees and their manager who work for the manager that manages the greatest number of employee (12) 
+
+58. List all pairs of employees that have the same last name. Make sure each combination is listed only once (5) 
+
+59. Select the name of each of two customers who have made at least one payment on the same date as the other.  Make sure that each pair of customers only appears once. (46)
+
+60. Find customers that share the same state and country.  The country must be one of the following: UK, Australia, Italy or Canada.  Remember that not all countries have states at all, so you need to substitute a character sting like ‘N/A’ for the state in those cases so that you can compare the states.
+
+61. Find all of the customers who have the same sales representative as some other customer, and either customer name has ‘Australian’ in it.  List each of the customers sharing a sales representative, and the name of the sales representative.  Order by the name of the first customer, then the second.  Do not show any combination more than once. (9) 
